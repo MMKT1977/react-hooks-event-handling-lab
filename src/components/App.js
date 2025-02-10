bac70ec65c103de9890c1bfec9fbd9593f7a7ccc
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-10 15:19:16 +0300

    created components and event listeners

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Keypad from "./Keypad";
+import EyesOnMe from "./EyesOnMe";
+
+function App(){
+
+    return(
+        <div>
+            <EyesOnMe  />
+            <Keypad />
+        </div>
+    )
+}
+export default App;
